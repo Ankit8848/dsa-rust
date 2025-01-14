8387c7e8e442690b8ba4f69f8c5c39ffa3eb85a7
ankit kumar jha <2100032609@kluniversity.in>
2025-01-14 23:27:41 +0530

    input parse add

--- data_structures/user_input_output/parsing_input/src/main.rs
+++ data_structures/user_input_output/parsing_input/src/main.rs
@@ -1,3 +1,21 @@
+// if you want to convert user input into a specific type.
+// use parse method.
+
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+
+    println!("Enter a number:");
+
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+
+    // Parse the input string into an integer
+    let number: i32 = input.trim().parse().expect("Please enter a valid number!");
+
+    println!("You entered: {}", number);
 }
+
+// input.trim().parse(): Converts the trimmed string into the desired type.
