928e019f17d101fe61d6faffca4f7bb471eebd5a
ankit kumar jha <2100032609@kluniversity.in>
2025-01-15 15:17:42 +0530

    number decrease in count per row

--- data_structures/star_patterns/problem_6/src/main.rs
+++ data_structures/star_patterns/problem_6/src/main.rs
@@ -1,3 +1,18 @@
+/*
+output-
+12345
+1234
+123
+12
+1
+*/
 fn main() {
-    println!("Hello, world!");
+    let rows = 5;
+
+    for i in (1..=rows).rev() {
+        for j in 1..=i {
+            print!("{}", j);
+        }
+        println!();
+    }
 }
