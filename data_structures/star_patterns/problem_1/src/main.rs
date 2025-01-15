463e7b4255a20090d7b467e3788337b42a46d42c
ankit kumar jha <2100032609@kluniversity.in>
2025-01-15 11:37:28 +0530

    5 star in 5 rows and column

--- data_structures/star_patterns/problem_1/src/main.rs
+++ data_structures/star_patterns/problem_1/src/main.rs
@@ -1,3 +1,19 @@
+/*
+output-
+*****
+*****
+*****
+*****
+*****
+*/ 
 fn main() {
-    println!("Hello, world!");
+    let rows = 5;
+    let cols = 5;
+
+    for _ in 0..rows {
+        for _ in 0..cols {
+            print!("*");
+        }
+        println!();
+    }
 }
