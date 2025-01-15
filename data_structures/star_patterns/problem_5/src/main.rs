72869f03f83b34e67f0c5fa3c6f840e0ab5b6f21
ankit kumar jha <2100032609@kluniversity.in>
2025-01-15 15:12:14 +0530

    number of stars decreases with each row

--- data_structures/star_patterns/problem_5/src/main.rs
+++ data_structures/star_patterns/problem_5/src/main.rs
@@ -1,3 +1,18 @@
+/*
+output-
+*****
+****
+***
+**
+*
+*/
 fn main() {
-    println!("Hello, world!");
+    let rows = 5;
+
+    for i in (1..=rows).rev() {
+        for _ in 0..i {
+            print!("*");
+        }
+        println!();
+    }
 }
