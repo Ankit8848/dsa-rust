a6a1136f305b737cb7d1331d810de4947f3faeac
ankit kumar jha <2100032609@kluniversity.in>
2025-01-15 11:56:30 +0530

    each row repeated number corresponding to the row number

--- data_structures/star_patterns/problem_4/src/main.rs
+++ data_structures/star_patterns/problem_4/src/main.rs
@@ -1,3 +1,18 @@
+/*
+output-
+1
+22
+333
+4444
+55555
+*/
 fn main() {
-    println!("Hello, world!");
+    let star = 5;
+
+    for i in 1..=star {
+        for _ in 1..=i {
+            print!("{}", i);
+        }
+        println!();
+    }
 }
