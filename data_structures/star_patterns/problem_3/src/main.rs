affc5eda3db3e885675a0f21b1732ded6c155ccb
ankit kumar jha <2100032609@kluniversity.in>
2025-01-15 11:52:13 +0530

    solution add

--- data_structures/star_patterns/problem_3/src/main.rs
+++ data_structures/star_patterns/problem_3/src/main.rs
@@ -1,3 +1,18 @@
+/*
+output-
+1
+12
+123
+1234
+12345
+*/
 fn main() {
-    println!("Hello, world!");
+    let star = 5;
+
+    for i in 1..=star {
+        for j in 1..=i {
+            print!("{}", j);
+        }
+        println!();
+    }
 }
