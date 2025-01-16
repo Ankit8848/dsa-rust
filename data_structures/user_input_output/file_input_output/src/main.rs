1507c60c34ca32a5dcece8e11f84c4cce0cfe784
ankit kumar jha <2100032609@kluniversity.in>
2025-01-16 19:24:12 +0530

    file write and read add

--- data_structures/user_input_output/file_input_output/src/main.rs
+++ data_structures/user_input_output/file_input_output/src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+//Rust also supports reading from and writing to files using the std::fs module.
+
+// writing to a file
+use std::fs::File;
+use std::io::Write;
+
+fn main() -> std::io::Result<()> {
+    let mut file = File::create("output.txt")?;
+    file.write_all(b"Hello, file!")?;
+    Ok(())
 }
+
+// reading from a file
+/*
+use std::fs::File;
+use std::io::{self, BufRead};
+
+fn main() -> std::io::Result<()> {
+    let file = File::open("input.txt")?;
+    let reader = io::BufReader::new(file);
+
+    for line in reader.lines() {
+        println!("{}", line?);
+    }
+
+    Ok(())
+}
+*/
